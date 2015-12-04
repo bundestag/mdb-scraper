59324c7ed1e26510048bcfe65ecf2ea5da0bb8fd
Friedrich Lindenberg <friedrich@pudo.org>
2015-12-04 18:38:14 +0100

    fix process bugs

--- run.sh
+++ run.sh
@@ -4,8 +4,7 @@ ARCHIVE=$DATA_PATH/bundestag/mdb
 POPOLOFILE=$ARCHIVE/popolo-`date +%Y%m%d`.json
 
 mkdir -p $ARCHIVE
+python scraper.py $POPOLOFILE
 
 aws s3 cp $POPOLOFILE s3://archive.pudo.org/bundestag/mdb/popolo-`date +%Y%m%d`.json
 aws s3 cp $POPOLOFILE s3://archive.pudo.org/bundestag/mdb/popolo-latest.json
-
-python scraper.py $POPOLOFILE
